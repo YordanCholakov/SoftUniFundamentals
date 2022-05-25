82fb557a01eb07adca3ecef6e8623c6729a3ac02
YordanCholakov <105978246+YordanCholakov@users.noreply.github.com>
2022-05-26 02:03:57 +0300

    Delete maximum_multiple.py

--- maximum_multiple.py
+++
@@ -1,8 +0,0 @@
-divisor = int(input())
-boundary = int(input())
-largest = 0
-for number in range(boundary, divisor-1, -1):
-    if number%divisor==0:
-        largest = number
-        print(largest)
-        break
