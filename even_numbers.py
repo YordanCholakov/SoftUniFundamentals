a3bd9b072ca7ce0d192bf70f4c7831273fc4aecd
YordanCholakov <105978246+YordanCholakov@users.noreply.github.com>
2022-05-26 02:02:36 +0300

    Delete even_numbers.py

--- even_numbers.py
+++
@@ -1,9 +0,0 @@
-numbers = int(input())
-
-for i in range(1, numbers+1):
-    current_number = int(input())
-    if current_number % 2 != 0:
-        print(f'{current_number} is odd!')
-        break
-else:
-    print(f'All numbers are even.')
